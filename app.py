827d2f02369415ce2ed97215dd8886463441e2d1
Jorge Rivero <riverojorge145@gmail.com>
2018-09-07 15:18:00 -0500

    Added a simple flask app

---
+++ app.py
@@ -0,0 +1,18 @@
+from firebase import firebase
+from flask import Flask
+from .forms import FirePut
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index(): 
+	return "<h1>Hello World!</h1>"
+
+@app.route("/testing")
+
+def testing(): 
+	return "<h1>This is another testing page!</h1>"
+
+if __name__ == '__main__':
+	app.run(debug=True)
