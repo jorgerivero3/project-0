c60f9c9a94b2d6b5f46a5189df730abe34576f22
Jorge Rivero <riverojorge145@gmail.com>
2018-09-13 21:45:44 -0500

    Added the virtaual env to the projectfile and added gitignore to ignore files not necessary.

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
 from firebase import firebase
-from flask import Flask
+from forms import RegistrationForm, LoginForm
+from flask import Flask, render_template, url_for, flash, redirect, request
+
+
+firebase = firebase.FirebaseApplication('https://project-0-1a188.firebaseio.com', authentication=None)
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'c435bd07880364149cdf9661f1994db4'
 
 @app.route('/')
 
 def index(): 
-	return "<h1>Hello World!</h1>"
+	return render_template('./index.html', name='App Title')
 
-@app.route("/testing")
+@app.route("/register")
+def register():
+	form = RegistrationForm()
+	return render_template("register.html", title="Register", form=form)
 
-def testing(): 
-	return "<h1>This is another testing page!</h1>"
+@app.route("/register/submit", methods=["POST"])
+def register_submit():
+	data = request
+	print(data)
+	return "Ayy lmao"
+
+@app.route("/login")
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash(f"Account created for {form.username.data}!", "success")
+		return redirect(url_for('index'))
+	return render_template('login.html', title="Login", form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
