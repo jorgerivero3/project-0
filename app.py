705f110ac3fc1d32806416a5f1bcd545d85449bd
what <emilyxia53@gmail.com>
2018-09-14 14:44:04 -0500

    Routing for login

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from firebase import firebase
 from forms import RegistrationForm, LoginForm
 from flask import Flask, render_template, url_for, flash, redirect, request
-
+from flask_login import LoginManager
 
 firebase = firebase.FirebaseApplication('https://project-0-1a188.firebaseio.com', authentication=None)
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'c435bd07880364149cdf9661f1994db4'
+login_manager = LoginManager(app)
 
 @app.route('/')
-
 def index(): 
 	return render_template('./index.html', name='App Title')
 
