f26b7f7068a5d4d7f8661c79c51a2d9a9886b725
Vincent Liu <Vincent@Vincents-MacBook-Air.local>
2018-09-19 18:12:16 -0500

    more update account updates

--- Application/routes.py
+++ Application/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from Application import app, db, bcrypt
-from Application.forms import RegistrationForm, LoginForm
+from Application.forms import RegistrationForm, LoginForm, UpdateInfo
 from Application.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -70,8 +70,18 @@ def loutout():
 	return redirect(url_for("home"))
 
 
-@app.route("/account")
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
+	form = UpdateInfo()
+	if form.validate_on_submit():
+		current_user.username = form.username.data
+		current_user.email = form.email.data
+		db.session.commit()
+		flash('Info Updated', 'success')
+		return redirect(url_for('account'))
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.email.data = current_user.email
 	image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-	return render_template('account.html', title="Account", image_file=image_file)
+	return render_template('account.html', title="Account", image_file=image_file, form=form)
