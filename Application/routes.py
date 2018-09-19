9dff08635373cd1a6ed732d43bf5cbd3399e1d91
Vincent Liu <Vincent@Vincents-MacBook-Air.local>
2018-09-18 22:39:04 -0500

    fixed some routing stuff and db stuff

--- Application/routes.py
+++ Application/routes.py
@@ -16,7 +16,11 @@ def register():
 		return redirect(url_for('home'))
 	form = RegistrationForm()
 	if form.validate_on_submit():
-		flash(f'Account created for {form.username.data}!', 'success')
+		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+		user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+		db.session.add(user)
+		db.session.commit()
+		flash('Account created! You can now log in', 'success')
 		return redirect(url_for('login'))
 	return render_template("register.html", title="Register", form=form)
 
