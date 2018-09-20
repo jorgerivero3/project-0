61009d9064c6baba97469fe14ff4bf23b07c74fe
Vincent Liu <Vincent@wireless-10-147-126-126.public.utexas.edu>
2018-09-20 11:37:10 -0500

    checks for admin privs

--- Application/routes.py
+++ Application/routes.py
@@ -146,7 +146,7 @@ def update_post(post_id):
 @login_required
 def delete_post(post_id):
 	post = Post.query.get_or_404(post_id)
-	if post.author != current_user:
+	if (current_user.admin != True) and (post.author != current_user):
 		abort(403)
 	db.session.delete(post)
 	db.session.commit()
