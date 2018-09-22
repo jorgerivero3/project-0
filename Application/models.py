3be496b2e5c655f89ef8af0abdc589ba938b0884
Vincent Liu <hliu38@hotmail.com>
2018-09-22 01:19:43 -0500

    updated db and stuff

--- Application/models.py
+++ Application/models.py
@@ -16,7 +16,7 @@ class User(db.Model, UserMixin):
 	email = db.Column(db.String(120), unique=True, nullable=False)
 	image_file = db.Column(db.LargeBinary)
 	password = db.Column(db.String(60), nullable=False)
-	posts = db.relationship('Post', backref='user_id', lazy=True)
+	posts = db.relationship('Post', backref='author', lazy=True)
 	admin = db.Column(db.Boolean, unique=False, default=True)
 
 	def get_reset_token(self, expires_sec=1800):
