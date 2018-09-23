4e2cab2730cfc5d5fe57773cef862a024f54021a
Vincent Liu <hliu38@hotmail.com>
2018-09-22 19:29:14 -0500

    fixed some default image stuff

--- Application/routes.py
+++ Application/routes.py
@@ -99,7 +99,8 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
-	return render_template('account.html', title="Account")
+	image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+	return render_template('account.html', title="Account", image_file=image_file)
 
 @app.route("/updateInfo", methods=['GET', 'POST'])
 @login_required
